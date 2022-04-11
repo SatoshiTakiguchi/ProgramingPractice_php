a679f9c5d1cf8a2daf232ba83f28448a5682f276
SatoshiTakiguchi <satoshi.takiguchi@tokyo.supersoftware.co.jp>
2022-04-11 17:24:37 +0900

    クラスの利用

--- php.php
+++ php.php
@@ -1,56 +1,69 @@
 
 <?php
-# 以下[桃，イチゴ]とする
-$price = [[],[]]; # [[桃の値段配列],[イチゴの値段配列]]
-# それぞれ値段を設定する
-for($i = 0; $i < 15; $i++){
-    $price[0][] = mt_rand(200,300);
-}
-for($i = 0; $i < 15; $i++){
-    $price[1][] = mt_rand(400,500);
-}
 
-# 最大値、最小値、合計値、平均値の初期化
-$max_price = [0,0];
-$min_price = [1000,1000];
-$sum_price = [0,0];
-$ave_price = [0,0];
-
-# 果物一つ一つについて
-for($i = 0; $i < 2; $i++){
-    $num = count($price[$i]); # 果物の個数を取得
-
-    #商品一つ一つについて
-    for($j = 0; $j < $num; $j++){
-        $p = $price[$i][$j]; # 変数を記述しやすくする
-        
-        # 合計金額の更新
-        $sum_price[$i] += $p;
-        
-        # 最高金額の更新
-        if($max_price[$i] < $p){
-            $max_price[$i] = $p;
-        }
-        # 最安値の更新
-        if($p < $min_price[$i]){
-            $min_price[$i] = $p;
+class Fruits
+{
+    public $name;
+    public $max_range_price;
+    public $min_range_price;
+    public $price = [];
+    
+    // 初期化
+    public function __construct($name,$num,$min_price,$max_price){
+        $this->name = $name;
+        $this->max_price = $min_price;
+        $this->min_price = $max_price;
+        for($i = 0; $i < $num; $i++){
+            $this->price[] = mt_rand($min_price,$max_price);
         }
     }
-    #平均金額の取得
-    $ave_price[$i] = $sum_price[$i] / $num;
     
+    // 値段と個数の再編成
+    public function reCreate($num,$min_price,$max_price){
+        $this->price = [];
+        for($i = 0; $i < $num; $i++){
+            $this->price[] = mt_rand($min_price,$max_price);
+        }
+    }
+    // 名前変更
+    public function setName($name){
+        $this->name = $name;
+    }
+    // 最高値取得
+    public function maxPrice(){
+        return max($this->price);
+    }
+    // 最安値取得
+    public function minPrice(){
+        return min($this->price);
+    }
+    // 平均価格取得
+    public function avePrice(){
+        return round(array_sum($this->price)/count($this->price));
+    }
+    // 結果の出力
+    public function addFruit($num){
+        for($i = 0; $i < $num; $i++){
+            $this->price[] = mt_rand($this->min_price,$this->max_price);
+        }
+    }
+    
+    // 統計結果出力
+    public function getStatistics(){
+        echo "{$this->name}の統計\n";
+        echo "最高値{$this->maxPrice()}円\n";
+        echo "最安値{$this->minPrice()}円\n";
+        echo "平均価格{$this->avePrice()}円\n";
+    }
 }
 
-echo 
-"桃\n",
-"最高値",$max_price[0],"\n",
-"最安値",$min_price[0],"\n",
-"平均価格",$ave_price[0],"\n";
-
-echo 
-"イチゴ\n",
-"最高値",$max_price[1],"\n",
-"最安値",$min_price[1],"\n",
-"平均価格",$ave_price[1],"\n";
+$momo = new Fruits("桃",15,200,300);
+$ichigo = new Fruits("イチゴ",15,400,500);
+$momo->getStatistics();
+$ichigo->getStatistics();
+echo "\nおまけ\n";
+$momo->setName("momo");
+$momo->reCreate(2,100,200);
+$momo->getStatistics();
 
 ?>

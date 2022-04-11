1475679ddab813781aa096b6ec98c4e755c1ee6a
SatoshiTakiguchi <satoshi.takiguchi@tokyo.supersoftware.co.jp>
2022-04-11 17:27:02 +0900

    クラスの利用

--- php.php
+++ php.php
@@ -4,15 +4,11 @@
 class Fruits
 {
     public $name;
-    public $max_range_price;
-    public $min_range_price;
     public $price = [];
     
     // 初期化
     public function __construct($name,$num,$min_price,$max_price){
         $this->name = $name;
-        $this->max_price = $min_price;
-        $this->min_price = $max_price;
         for($i = 0; $i < $num; $i++){
             $this->price[] = mt_rand($min_price,$max_price);
         }
@@ -40,12 +36,6 @@ class Fruits
     // 平均価格取得
     public function avePrice(){
         return round(array_sum($this->price)/count($this->price));
-    }
-    // 結果の出力
-    public function addFruit($num){
-        for($i = 0; $i < $num; $i++){
-            $this->price[] = mt_rand($this->min_price,$this->max_price);
-        }
     }
     
     // 統計結果出力
